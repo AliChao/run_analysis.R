a3431642e2822cdcba4eea687ff7da1734d2f451
AliChao <alichao78@gmail.com>
2014-05-24 15:47:58 +0800

    change a lot

--- run_analysis.R
+++ run_analysis.R
@@ -1,46 +1,60 @@
-##load X_test,Y_test,X_train,Y_train
+## 1.Merges the training and the test sets to create one data set.
+##load X_test,Y_test,X_train,Y_train,subject_test,subject_train
 X_test <- read.table("G:/R_code/data/UCI HAR Dataset/test/X_test.txt", quote="\"")
 X_train <- read.table("G:/R_code/data/UCI HAR Dataset/train/X_train.txt", quote="\"")
 Y_test <- read.table("G:/R_code/data/UCI HAR Dataset/test/Y_test.txt", quote="\"")
 Y_train <- read.table("G:/R_code/data/UCI HAR Dataset/train/Y_train.txt", quote="\"")
+subject_test <- read.table("G:/R_code/data/UCI HAR Dataset/test/subject_test.txt", quote="\"")
+subject_train <- read.table("G:/R_code/data/UCI HAR Dataset/train/subject_train.txt", quote="\"")
+
+##change the colnames 
+features <- read.table("G:/R_code/data/UCI HAR Dataset/features.txt", quote="\"")
+colnames(X_test) <- c(as.character(features$V2))
+colnames(X_train) <- c(as.character(features$V2))
+colnames(Y_test) <- c("labels_ID")
+colnames(Y_train) <- c("labels_ID")
+colnames(subject_test) <-c ("subject")
+colnames(subject_train) <-c ("subject")
 
 ## combine X_test and Y_test to XY_test, X_train and Y_train to XY_train
-XY_train<- cbind (Y_train,X_train)
-XY_test<-cbind(Y_test,X_test)
+XY_test<-cbind(subject_test,Y_test,X_test)
+XY_train<- cbind (subject_train,Y_train,X_train)
 
 ##merge XY_train and XY_test to XY_test_train
 XY_test_train <- rbind(XY_test,XY_train)
 
-##rename the XY_test_train 's colnames 
-colnames(XY_test_train) <- c("number",colnames(XY_test_train[2:562]))
-
-##load activity_labels and merge activity_labels and XY_test_train to XY2_test_train
-activity_labels <- read.table("G:/R_code/data/UCI HAR Dataset/activity_labels.txt", quote="\"")
-XY2_test_train <- merge.data.frame(x=XY_test_train,y=activity_labels,by.x="number",by.y="V1",sort=F)
-
-##rename the XY2_test_train's colnames
-features <- read.table("G:/R_code/data/UCI HAR Dataset/features.txt", quote="\"")
-colnames(XY2_test_train) <- c("Activity_ID",as.character(features$V2),"Activity_labels")
-
-## put the labels_names to col 2 
-XY3_test_train <- XY2_test_train[,c(1,563,2:562)]
-
+## 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
 ## sorting mean and std
 meanstd <-grep("std()|mean()",features$V2)
 meanstd2 <- meanstd[c(-47,-48,-49,-57,-58,-65,-66,-67,-70,-73,-76,-79)]
 meanstd3 <-c(1,2,meanstd2+c(2))
-XY4_test_train <- XY3_test_train[,meanstd3]
+XY2_test_train <- XY_test_train[,meanstd3]
 
-#make new data set for average per vaiaable per subject
-mean <- tapply(XY4_test_train[,3],XY4_test_train$Activity_ID,mean)
-for (i in 4:69){
-  m<-tapply(XY4_test_train[,i],XY4_test_train$Activity_ID,mean)
-  mean <- cbind(mean,m)
-}
-#add colnames and rownames
-colnamesmean <-colnames(XY4_test_train[3:69])
-colnames(mean) <-colnamesmean
-rownames(mean) <- activity_labels$V2
+## 3.Uses descriptive activity names to name the activities in 
+##the data set(have done in stage 1)
 
-write.table(XY4_test_train, "c:/tidydata1.txt", sep="\t")
-write.table(mean, "c:/tidydatamean.txt", sep="\t")
+## 4.Appropriately labels the data set with descriptive activity names. 
+##load activity_labels and merge activity_labels and XY2_test_train to XY2_test_train
+activity_labels <- read.table("G:/R_code/data/UCI HAR Dataset/activity_labels.txt", quote="\"")
+colnames(activity_labels) <- c("labels_ID","Activity_labels")
+XY2_test_train <- merge.data.frame(x=XY2_test_train,y=activity_labels,by.x="labels_ID",by.y="labels_ID",sort=F)
+
+## put the labels_names to col 2 
+XY3_test_train <- XY2_test_train[,c(1,70,2,3:69)]
+
+## 5. Creates a second, independent tidy data set with the
+##average of each variable for each activity and each subject. 
+## delete the labels_names 
+XY4_test_train <- XY3_test_train[,c(1,3:70)] 
+XY4_test_train <-aggregate(XY4_test_train[,c(3:69)],by=list(labels_ID=XY4_test_train$labels_ID,subject=XY4_test_train$subject),FUN=mean)
+
+## add the labels_names back  
+XY5_test_train <-merge.data.frame(x=activity_labels,y=XY4_test_train,by.x="labels_ID",by.y="labels_ID",sort=F)
+
+## order the tidy data 
+XY6_test_train <-XY5_test_train[order(XY5_test_train$labels_ID,XY5_test_train$subject),]
+rownames(XY6_test_train) <-c()
+
+#outcome
+write.table(XY3_test_train, "c:/tidydata1.txt", sep="\t")
+write.table(XY6_test_train, "c:/tidydatamean.txt", sep="\t")
